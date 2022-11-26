45bd049d1a848a28892ce546edc4cac200239a28
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2022-11-26 23:02:20 +0100

    20221126_3

--- frontend/src/js/helpers/scrolling.js
+++ frontend/src/js/helpers/scrolling.js
@@ -2,5 +2,8 @@ const scrollContainer = document.querySelector(".gsoa-fp-outer");
 
 scrollContainer.addEventListener("wheel", (evt) => {
   evt.preventDefault();
+  if (evt.deltaX !== 0) {
+    scrollContainer.scrollLeft += evt.deltaX;
+  }
   scrollContainer.scrollLeft += evt.deltaY;
 });
